4c6b63af98b9d5dd7ac35a2cee8fc6aeee5ecf76
alokmishrawaviz <alokmishra.waviz@gmail.com>
2018-02-15 15:26:08 +0530

    Add files via upload

---
+++ DecendingOrder.java
@@ -0,0 +1,36 @@
+package com.Array;
+
+import java.util.Scanner;
+
+public class DecendingOrder {
+
+	public static void main(String[] args) {
+		
+		Scanner  sc=new Scanner(System.in);
+		System.out.println("Enter the size of array");
+		int n=sc.nextInt();
+		int temp=0;
+		int[] arr=new int[n];
+		System.out.println("Enter the element of array");
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++){
+			for(int j=i+1;j<n;j++){
+				
+				 if(arr[i]<(){
+					 
+					 
+					 
+				 }
+				 }
+			}
+		}
+		for(int i=0;i<n-1;i++){
+			System.out.println(arr[i]);
+		}
+		System.out.println(arr[n-1]);
+
+	}
+
+}
